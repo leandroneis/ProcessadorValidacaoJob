735b62209c22687c4dbaa08053a02d558f11ca68
Leandro Neis <lgn200@gmail.com>
2024-08-29 15:10:16 -0300

    47. Prática: Processadores de validação

--- src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
+++ src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
@@ -1,16 +1,42 @@
 package com.springbatch.processadorvalidacao.processor;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.validator.BeanValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidationException;
+import org.springframework.batch.item.validator.Validator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.springbatch.processadorvalidacao.dominio.Cliente;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 public class ProcessadorValidacaoProcessorConfig {
-	@Bean
-	public ItemProcessor<Cliente, Cliente> procesadorValidacaoProcessor() {
-		// TODO: Implementar a lógica aqui...
-		return cliente -> cliente;
-	}
+
+    private Set<String> emails = new HashSet<>();
+
+
+    @Bean
+    public ItemProcessor<Cliente, Cliente> procesadorValidacaoProcessor() {
+//		BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<>();
+//		processor.setFilter(true);
+        ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<>();
+        processor.setValidator(validator());
+		processor.setFilter(true);
+        return processor;
+    }
+
+    private Validator<Cliente> validator() {
+        return new Validator<Cliente>() {
+            @Override
+            public void validate(Cliente cliente) throws ValidationException {
+                if (emails.contains(cliente.getEmail()))
+                    throw new ValidationException(String.format("O cliente %s já foi processado!", cliente.getEmail()));
+				emails.add(cliente.getEmail());
+            }
+        };
+    }
 }
